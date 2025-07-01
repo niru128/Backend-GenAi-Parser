aa0130fcaf677b51c368bbf0e04371746eb0a2b7
Niranjan <niranjancb2035@gmail.com>
2025-07-01 09:26:01 +0530

    Removed duplicated files from root, keeping only backend folder

--- routes/VisaRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getVisaRequirements, postVisaRequirements } from '../controllers/VisaController.js';
-const router = express.Router();
-
-
-router.post('/' , postVisaRequirements);  
-router.get('/:country' , getVisaRequirements);
-export default router;
--- utils/normalise.js
+++
@@ -1,8 +0,0 @@
-const normalized = (data)=>{
-    return {
-        visa_required : data.visa_required ?? true,
-        documents : data.documents ?? ['passport']
-    };
-};
-
-export default normalized;
